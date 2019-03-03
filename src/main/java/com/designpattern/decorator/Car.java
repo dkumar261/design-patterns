ae382be07d99a34ff838595603f9990050452c4a
Dinesh Kumar <dkumar261@csc.com>
2019-03-03 11:44:45 +0530

    Adding Decorator Pattern.

---
+++ src/main/java/com/designpattern/decorator/Car.java
@@ -0,0 +1,6 @@
+package com.designpattern.decorator;
+
+public interface Car {
+
+	public void assemble();
+}
--- src/main/java/com/designpattern/singleton/Test.java
+++ src/main/java/com/designpattern/singleton/Test.java
@@ -1,7 +1,5 @@
 package com.designpattern.singleton;
 
-
-
 public class Test{
 	
 	Display desplay = Display.getDisplay();
