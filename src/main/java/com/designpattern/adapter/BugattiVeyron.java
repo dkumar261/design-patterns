6d4b98c39f1323b520a043a57f9a59eb18f5365d
Dinesh Kumar <krsnvicky@gmail.com>
2020-02-15 23:17:23 +0300

    Adapter is added

---
+++ src/main/java/com/designpattern/adapter/BugattiVeyron.java
@@ -0,0 +1,9 @@
+package com.designpattern.adapter;
+
+public class BugattiVeyron implements Movable {
+	  
+    @Override
+    public double getSpeed() {
+        return 268;
+    }
+}
---
+++ src/main/java/com/designpattern/adapter/MovableAdapterImpl.java
@@ -0,0 +1,21 @@
+package com.designpattern.adapter;
+
+public class MovableAdapterImpl implements MovableAdapter {
+    private Movable luxuryCars;
+     
+    // standard constructors
+ 
+    public MovableAdapterImpl(Movable bugattiVeyron) {
+    	this.luxuryCars= bugattiVeyron;
+    
+    }
+
+	@Override
+    public double getSpeed() {
+        return convertMPHtoKMPH(luxuryCars.getSpeed());
+    }
+     
+    private double convertMPHtoKMPH(double mph) {
+        return mph * 1.60934;
+    }
+}
