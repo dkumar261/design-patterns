3727fdee9c7f4f239ff416f1e8cd9c513b5cc6b5
Dinesh Kumar <krsnvicky@gmail.com>
2020-11-05 21:45:05 +0530

    Serial Version UID

---
+++ src/main/java/com/designpattern/util/serialversion/uid/Address.java
@@ -0,0 +1,41 @@
+package com.designpattern.util.serialversion.uid;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 4147476221638309739L;
+
+    String street;
+    String country;
+
+    String ab;
+    public Address(String street, String country) {
+        this.street = street;
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
